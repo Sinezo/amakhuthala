91a0c24d67653705253ee06ba6a70980fef55016
Sinezo <lusawanasinethemba@gmail.com>
2023-02-26 11:42:34 +0200

    saving my changes

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ function MENU(){
         document.querySelector('.content-wrapper').style.marginLeft='-250px';
         document.querySelector('.content-wrapper').style.opacity='.5';
         document.querySelector('.menu-wrapper').style.width='100%';
+
+        document.querySelector('.bar-one').style.transform='rotate(45deg)';
+        document.querySelector('.bar-one').style.marginBottom='1px';
+        document.querySelector('.bar-two').style.display='none';
+        document.querySelector('.bar-three').style.transform='rotate(-45deg)';
+        document.querySelector('.bar-three').style.marginTop='-3px';
         window.location="#";
 
         menu_show=0;
@@ -13,6 +19,13 @@ function MENU(){
         document.querySelector('.content-wrapper').style.margin='0';
         document.querySelector('.content-wrapper').style.opacity='1';
         document.querySelector('.menu-wrapper').style.width='0';
+
+        document.querySelector('.bar-one').style.transform='rotate(0)';
+        document.querySelector('.bar-one').style.marginBottom='3px';
+        document.querySelector('.bar-two').style.display='block';
+        document.querySelector('.bar-three').style.transform='rotate(0)';
+        document.querySelector('.bar-three').style.marginTop='0';
+
         menu_show=1;
     }
 }
