f29237e4d086129e855a42d897b67777a50865cb
Sinezo <lusawanasinethemba@gmail.com>
2023-04-24 21:38:20 +0200

    saving

--- index.js
+++ index.js
@@ -16,3 +16,21 @@ close_icon.addEventListener('click', function(){
     document.querySelector('.menu-wrapper').style.overflow='hidden';
 
 });
+
+//window scroll
+window.onscroll=function(){
+    scrolling();
+};
+function scrolling(){
+    if(document.body.scrollTop > 0 || document.documentElement.scrollTop >100){
+        document.querySelector('.nav-small-screen').style.backgroundColor='white';
+        document.querySelector('.nav-small-screen').style.position='fixed';
+        document.querySelector('.nav-small-screen').style.boxShadow='1px 1px 3px lightgrey';
+        
+
+    }
+    else{
+        document.querySelector('.nav-small-screen').style.backgroundColor='unset';
+        document.querySelector('.nav-small-screen').style.boxShadow='none';
+    }
+}
